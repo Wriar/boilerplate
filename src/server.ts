da9629a1a190b541ed40ce81c7080b7fc9bce44c
Wriar <81789627+Wriar@users.noreply.github.com>
2024-10-16 21:55:09 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -41,7 +41,7 @@ app.use(session({
 app.use(cookieParser());
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './bin/views'));
+app.set('views', path.join(__dirname, './views'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
